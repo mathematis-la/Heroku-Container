a9e877017f9a66aba460aeb8d1968a8b7ccfedc6
RizkiMufrizal <mufrizalrizki@gmail.com>
2018-02-11 23:39:05 +0700

    Initial Project

---
+++ src/test/kotlin/org/rizki/mufrizal/heroku/container/HerokuContainerApplicationTests.kt
@@ -0,0 +1,16 @@
+package org.rizki.mufrizal.heroku.container
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class HerokuContainerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
